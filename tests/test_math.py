55cd879e8f782df0a0b658a92465fb819bac215a
Cevdet Ayaz <9591165+cevdetayaz@users.noreply.github.com>
2022-10-21 23:24:16 +0300

    ini config ve markers eklendi.

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,14 +1,17 @@
 import pytest
 
+@pytest.mark.math
 def test_one_plus_one():
     assert 1 + 1 == 2
-
+    
+@pytest.mark.math
 def test_one_plus_two():
     a = 1
     b = 2
     c = 3
     assert a + b == c
-    
+
+@pytest.mark.math   
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1/ 0
@@ -24,12 +27,15 @@ def test_divide_by_zero():
 # negative by a negative
 # multiply floating point numbers
 
+@pytest.mark.math
 def test_multiply_two_positive_ints():
     assert 2 * 3 == 6
 
+@pytest.mark.math
 def test_multiply_identity():
     assert 1 * 99 == 99
 
+@pytest.mark.math
 def test_multiply_by_zero():
     assert 0 * 100 == 0
 
@@ -46,6 +52,7 @@ products = [
     (2.5, 6.7, 16.75),   # floats
 ]
 
+@pytest.mark.math
 @pytest.mark.parametrize('a, b, product', products)
 def test_multiplication(a, b, product):
     assert a * b == product
